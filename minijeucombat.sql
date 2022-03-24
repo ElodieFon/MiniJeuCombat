7a3cd9afffd6f1b155714999f5c96d95770a8a63
elodie fontaine <e.fontaine.1991@hotmail.fr>
2022-03-24 13:58:12 +0100

    remplacement fichier sql

--- minijeucombat.sql
+++ minijeucombat.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : ven. 04 mars 2022 à 16:53
--- Version du serveur : 5.7.36
--- Version de PHP : 8.0.13
+-- Généré le : jeu. 24 mars 2022 à 12:56
+-- Version du serveur :  5.7.31
+-- Version de PHP : 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,20 +33,25 @@ CREATE TABLE IF NOT EXISTS `personnages` (
   `nom` varchar(50) NOT NULL,
   `degats` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
   `dodo` int(4) UNSIGNED DEFAULT '0',
-  `type` enum('magicien','guerrier') NOT NULL,
+  `type` varchar(100) NOT NULL,
   `atout` tinyint(3) UNSIGNED NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
+  `lvl` tinyint(3) UNSIGNED NOT NULL DEFAULT '1',
+  `exp` tinyint(2) UNSIGNED NOT NULL DEFAULT '0',
+  `nbCoup` tinyint(1) UNSIGNED NOT NULL DEFAULT '0',
+  `dateCoup` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nom` (`nom`)
+) ENGINE=MyISAM AUTO_INCREMENT=35 DEFAULT CHARSET=utf8;
 
 --
 -- Déchargement des données de la table `personnages`
 --
 
-INSERT INTO `personnages` (`id`, `nom`, `degats`, `dodo`, `type`, `atout`) VALUES
-(16, 'nounours4', 3, 0, 'guerrier', 4),
-(20, 'magicat', 5, 0, 'magicien', 4),
-(18, 'nounours2', 2, 1646407085, 'guerrier', 4),
-(21, 'calis', 0, NULL, 'magicien', 0);
+INSERT INTO `personnages` (`id`, `nom`, `degats`, `dodo`, `type`, `atout`, `lvl`, `exp`, `nbCoup`, `dateCoup`) VALUES
+(34, 'clis', 0, NULL, 'magicien', 1, 1, 0, 0, NULL),
+(33, 'chasseur', 60, 0, 'chasseur', 0, 1, 0, 0, NULL),
+(32, 'guerrier', 91, 0, 'guerrier', 4, 1, 0, 0, NULL),
+(30, 'magicien', 65, 1647531725, 'magicien', 0, 1, 0, 0, NULL);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
